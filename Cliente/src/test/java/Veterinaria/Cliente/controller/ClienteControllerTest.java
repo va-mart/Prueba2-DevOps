feaa49c59be4d82dc001b5ada91d15013fafc549
VictoriaArias-i <victoria.arias.me@gmail.com>
2026-05-23 23:20:24 -0400

    Add AutoConfigureMock

--- Cliente/src/test/java/Veterinaria/Cliente/controller/ClienteControllerTest.java
+++ Cliente/src/test/java/Veterinaria/Cliente/controller/ClienteControllerTest.java
@@ -11,6 +11,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
@@ -26,6 +27,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(ClienteController.class)
+@AutoConfigureMockMvc(addFilters = false)
 @WithMockUser
 class ClienteControllerTest {
 
